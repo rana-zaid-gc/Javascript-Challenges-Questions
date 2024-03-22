bced76cb7d7241fe26b4eccd82e8146d7d02c88d
rana zaid <itszaidrajpoot@gmail.com>
2024-03-23 02:55:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//create an array of numbers and take an input from the user to add it into the array
+
+
+
+let arr = [2,4,5,6,7,90,78,48]
+let a = parent(prompt("enter the number"))
+arr.push(a)
+console.log(arr)
+
+
+//keep adding numbers to the array in above question until user adds the 0
