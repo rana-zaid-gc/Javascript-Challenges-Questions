c1d44982f12ca07fc216bf6889594b02a1296a0a
rana zaid <itszaidrajpoot@gmail.com>
2024-03-24 00:06:59 +0500

    add some code

--- main.js
+++ main.js
@@ -2,10 +2,47 @@
 
 
 
-let arr = [2,4,5,6,7,90,78,48]
-let a = parent(prompt("enter the number"))
-arr.push(a)
-console.log(arr)
+// let arr = [2,4,5,6,7,90,78,48]
+// let a = parent(prompt("enter the number"))
+// arr.push(a)
+// console.log(arr)
 
 
 //keep adding numbers to the array in above question until user adds the 0
+
+// let  arr = [34,67,90,67]
+// let z;
+// do {
+//    let z = parseInt(prompt("enter the number"))
+//     arr.push(z)
+
+// }
+// while(z=!0)
+// console.log(arr)
+
+
+//filter for numbers divisble by 10 from a given array
+
+// let arr = [60,90,78,68,34]
+// let f = arr.filter((ele) =>{
+//  return (ele%10==0)
+// })
+// console.log(f)
+
+
+//create an array of square of given numbers
+
+// let arr = [45,67,89,34]
+// let square = arr.map((elements) => {
+//     return (elements*elements)
+// })
+// console.log(square)
+
+
+//use reduce to calculate factorial of a given number from an array of first n natural numbers
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let fac = arr.reduce((num1,num2) => {
+//     return(num1*num2)
+// })
+// console.log(fac)
